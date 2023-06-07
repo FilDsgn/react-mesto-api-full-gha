48098dd776e9f3321beea068c0d0118292d257b8
Alexander Filimonov <fil.dsgn@yandex.ru>
2023-06-07 20:09:18 +0300

    dev: add frontend

---
+++ frontend/src/components/App.js
@@ -0,0 +1,364 @@
+import React from "react";
+import { Routes, Route, Navigate, useNavigate } from "react-router-dom";
+
+import Header from "./Header.js";
+import Main from "./Main.js";
+import Footer from "./Footer.js";
+import DeleteCardPopup from "./DeleteCardPopup.js";
+import ImagePopup from "./ImagePopup.js";
+import EditProfilePopup from "./EditProfilePopup.js";
+import EditAvatarPopup from "./EditAvatarPopup.js";
+import AddPlacePopup from "./AddPlacePopup.js";
+import api from "../utils/api.js";
+import { CurrentUserContext } from "../contexts/CurrentUserContext.js";
+import Register from "./Register.js";
+import Login from "./Login.js";
+import InfoTooltip from "./InfoTooltip.js";
+import * as auth from "../utils/auth.js";
+
+function App() {
+  const [isEditProfilePopupOpen, setIsEditProfilePopupOpen] =
+    React.useState(false);
+  const [isAddPlacePopupOpen, setIsAddPlacePopupOpen] = React.useState(false);
+  const [isEditAvatarPopupOpen, setIsEditAvatarPopupOpen] =
+    React.useState(false);
+  const [isConfirmPopupOpen, setIsConfirmPopupOpen] = React.useState(false);
+  const [isImagePopupOpen, setIsImagePopupOpen] = React.useState(false);
+  const [selectedCard, setSelectedCard] = React.useState({});
+  const [currentUser, setCurrentUser] = React.useState({});
+  const [isLoading, setIsLoading] = React.useState(false);
+
+  const [loggedIn, setLoggedIn] = React.useState(false);
+  const [registered, setRegistered] = React.useState(false);
+  const [userEmail, setUserEmail] = React.useState("");
+
+  const [isInfoTooltipOpen, setIsInfoTooltipOpen] = React.useState(false);
+
+  const [cards, setCards] = React.useState([]);
+
+  const navigate = useNavigate();
+
+  const isPopupOpen =
+    isEditProfilePopupOpen ||
+    isAddPlacePopupOpen ||
+    isEditAvatarPopupOpen ||
+    isConfirmPopupOpen ||
+    isImagePopupOpen ||
+    isInfoTooltipOpen;
+
+  React.useEffect(() => {
+    api
+      .getCardList()
+      .then((cardList) => {
+        setCards(cardList);
+      })
+      .catch((err) => console.log(err));
+
+    api
+      .getUserInfo()
+      .then((userInfo) => {
+        setCurrentUser(userInfo);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  function closeAllPopups() {
+    setIsEditProfilePopupOpen(false);
+    setIsAddPlacePopupOpen(false);
+    setIsEditAvatarPopupOpen(false);
+    setIsImagePopupOpen(false);
+    setIsConfirmPopupOpen(false);
+    setIsInfoTooltipOpen(false);
+  }
+
+  function closeOnOverlayClick(e) {
+    if (e.target === e.currentTarget) {
+      closeAllPopups();
+    }
+  }
+
+  const closePopupsOnEsc = React.useCallback((e) => {
+    if (e.key === "Escape") {
+      closeAllPopups();
+    }
+  }, []);
+
+  React.useEffect(() => {
+    if (isPopupOpen) {
+      document.addEventListener("keydown", closePopupsOnEsc);
+      return () => document.removeEventListener("keydown", closePopupsOnEsc);
+    }
+  }, [isPopupOpen, closePopupsOnEsc]);
+
+  function handleTransitionEnd() {
+    if (!isPopupOpen) {
+      setSelectedCard({});
+    }
+  }
+
+  function handleCardLike(card) {
+    const isLiked = card.likes.some((like) => like._id === currentUser._id);
+
+    api
+      .changeLikeCardStatus(card.id, isLiked)
+      .then((newCard) => {
+        setCards((state) =>
+          state.map((c) => (c._id === card.id ? newCard : c))
+        );
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  function handleCardDelete(card) {
+    const isOwn = card.owner._id === currentUser._id;
+    setIsLoading(true);
+
+    if (isOwn) {
+      api
+        .deleteCard(card.id)
+        .then(() => {
+          setCards((state) => state.filter((c) => c._id !== card.id));
+          closeAllPopups();
+        })
+        .catch((err) => {
+          console.log(err);
+        })
+        .finally(() => setIsLoading(false));
+    }
+  }
+
+  function handleCardDeleteClick(card) {
+    setIsConfirmPopupOpen(true);
+    setSelectedCard(card);
+  }
+
+  function handleCardImageClick(card) {
+    setIsImagePopupOpen(true);
+    setSelectedCard(card);
+  }
+
+  function handleUpdateUser(updateUserInfo) {
+    setIsLoading(true);
+    api
+      .setUserInfo(updateUserInfo)
+      .then((data) => {
+        setCurrentUser(data);
+        closeAllPopups();
+      })
+      .catch((err) => console.log(err))
+      .finally(() => setIsLoading(false));
+  }
+
+  function handleUpdateAvatar(updateUserAvatar) {
+    setIsLoading(true);
+    api
+      .setUserAvatar(updateUserAvatar)
+      .then((data) => {
+        setCurrentUser(data);
+        closeAllPopups();
+      })
+      .catch((err) => console.log(err))
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }
+
+  function handleAddPlaceSubmit(newCard) {
+    setIsLoading(true);
+    api
+      .createCard(newCard)
+      .then((card) => {
+        setCards([card, ...cards]);
+        closeAllPopups();
+      })
+      .catch((err) => console.log(err))
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }
+
+  function handleLogin(email) {
+    setUserEmail(email);
+    setLoggedIn(true);
+  }
+
+  function handleLoginSubmit(password, email) {
+    setIsLoading(true);
+    auth
+      .authorize(password, email)
+      .then((data) => {
+        if (data.token) {
+          localStorage.setItem("token", data.token);
+          setUserEmail(email);
+          setLoggedIn(true);
+          navigate("/");
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }
+
+  function handleRegisterSubmit(password, email) {
+    setIsLoading(true);
+    auth
+      .register(password, email)
+      .then((data) => {
+        navigate("/sign-in");
+        setRegistered(true);
+        handleTooltipOpen();
+      })
+      .catch((err) => {
+        setRegistered(false);
+        handleTooltipOpen();
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }
+
+  function handleTokenCheck() {
+    const token = localStorage.getItem("token");
+    if (!token) {
+      return;
+    }
+
+    auth
+      .getContent(token)
+      .then((res) => {
+        if (res) {
+          setLoggedIn(true);
+          handleLogin(res.data.email);
+          navigate("/");
+        }
+      })
+      .catch((err) => console.log(err));
+  }
+
+  function handleLogout() {
+    localStorage.removeItem("token");
+    setLoggedIn(false);
+    setUserEmail("");
+  }
+
+  function handleTooltipOpen() {
+    setIsInfoTooltipOpen(true);
+  }
+
+  return (
+    <CurrentUserContext.Provider value={currentUser}>
+      <div className="App">
+        <div className="page">
+          <Header
+            userEmail={userEmail}
+            isLoggedIn={loggedIn}
+            onLogout={handleLogout}
+          />
+
+          <Routes>
+            <Route
+              path="/"
+              element={
+                loggedIn ? (
+                  <Main
+                    cards={cards}
+                    onEditProfile={setIsEditProfilePopupOpen}
+                    onAddPlace={setIsAddPlacePopupOpen}
+                    onEditAvatar={setIsEditAvatarPopupOpen}
+                    onCardClick={handleCardImageClick}
+                    onCardLike={handleCardLike}
+                    onCardDelete={handleCardDeleteClick}
+                  />
+                ) : (
+                  <Navigate to="/sign-in" replace />
+                )
+              }
+            />
+
+            <Route
+              path="/sign-up"
+              element={
+                <Register
+                  onSubmit={handleRegisterSubmit}
+                  onTokenCheck={handleTokenCheck}
+                  onLoading={isLoading}
+                />
+              }
+            />
+            <Route
+              path="/sign-in"
+              element={
+                <Login
+                  onSubmit={handleLoginSubmit}
+                  onTokenCheck={handleTokenCheck}
+                  onLoading={isLoading}
+                />
+              }
+            />
+            <Route path="*" element={<h2>Not Found</h2>} />
+          </Routes>
+
+          {loggedIn && <Footer />}
+
+          <EditProfilePopup
+            isOpen={isEditProfilePopupOpen}
+            onClose={closeAllPopups}
+            onOverlayClick={closeOnOverlayClick}
+            onUpdateUser={handleUpdateUser}
+            onLoading={isLoading}
+          />
+
+          <EditAvatarPopup
+            isOpen={isEditAvatarPopupOpen}
+            onClose={closeAllPopups}
+            onOverlayClick={closeOnOverlayClick}
+            onUpdateAvatar={handleUpdateAvatar}
+            onLoading={isLoading}
+          />
+
+          <AddPlacePopup
+            isOpen={isAddPlacePopupOpen}
+            onClose={closeAllPopups}
+            onOverlayClick={closeOnOverlayClick}
+            onAddPlace={handleAddPlaceSubmit}
+            onLoading={isLoading}
+          />
+
+          <ImagePopup
+            card={selectedCard}
+            isOpen={isImagePopupOpen}
+            onClose={closeAllPopups}
+            onOverlayClick={closeOnOverlayClick}
+            onTransitionEnd={handleTransitionEnd}
+          />
+
+          <DeleteCardPopup
+            card={selectedCard}
+            isOpen={isConfirmPopupOpen}
+            onClose={closeAllPopups}
+            onOverlayClick={closeOnOverlayClick}
+            onTransitionEnd={handleTransitionEnd}
+            onCardDelete={handleCardDelete}
+            onLoading={isLoading}
+          />
+
+          <InfoTooltip
+            name="info-tooltip"
+            isOpen={isInfoTooltipOpen}
+            registered={registered}
+            onClose={closeAllPopups}
+            onOverlayClick={closeOnOverlayClick}
+            onTransitionEnd={handleTransitionEnd}
+          />
+        </div>
+      </div>
+    </CurrentUserContext.Provider>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/AuthForm.js
@@ -0,0 +1,49 @@
+import { Link } from "react-router-dom";
+import { forwardRef } from "react";
+
+const AuthForm = forwardRef(
+  (
+    {
+      title,
+      name,
+      buttonText,
+      buttonTextOnLoading,
+      linkText = "",
+      handleSubmit,
+      isValid,
+      onLoading,
+      children,
+    },
+    ref
+  ) => {
+    return (
+      <form
+        action="#"
+        name={name}
+        onSubmit={handleSubmit}
+        noValidate
+        className="auth-form"
+        ref={ref}
+      >
+        <h2 className="auth-form__title">{title}</h2>
+
+        {children}
+
+        <button
+          type="submit"
+          aria-label=""
+          className={`auth-form__button ${
+            !isValid && "auth-form__button_disabled"
+          }`}
+        >
+          {isValid && onLoading ? buttonTextOnLoading : buttonText}
+        </button>
+        <Link to="/sign-in" className="auth-form__link">
+          {linkText}
+        </Link>
+      </form>
+    );
+  }
+);
+
+export default AuthForm;
