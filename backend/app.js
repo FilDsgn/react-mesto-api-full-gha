8027afd5ba2df48df3c1b01e82231d7ef3772e97
Alexander Filimonov <fil.dsgn@yandex.ru>
2023-06-09 21:22:59 +0300

    fix

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const cors = require('cors');
+// const cors = require('cors');
 
 const { PORT = 3000 } = process.env;
 const { errors } = require('celebrate');
